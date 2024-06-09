1900b6317d797eced05102a2001d47daa95f5914
suyogya dhoj <suyogyaadi@gmail.com>
2024-06-09 11:42:54 +0545

    authorization (config) in dashboard and update

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -2,36 +2,43 @@ import axios from 'axios';
 
 //creating backend config
 const Api = axios.create({
-    baseURL :"http://localhost:5000",
-    withCredentials : true,
-    headers :{
+    baseURL: "http://localhost:5000",
+    withCredentials: true,
+    headers: {
         "Content-Type": 'multipart/form-data'
     }
 })
 
+// make a config
+const config = {
+    headers: {
+        'authorization' : `Bearer ${localStorage.getItem('token')}`
+    }
+}
+
 // Test API
-export const testApi = ()=> Api.get('/test')
+export const testApi = () => Api.get('/test')
 
 //register API
-export const registerUserApi = (data)=> Api.post('/api/user/create',data)
+export const registerUserApi = (data) => Api.post('/api/user/create', data)
 
 //Login API
-export const loginUserApi = (data) => Api.post('/api/user/login',data)
+export const loginUserApi = (data) => Api.post('/api/user/login', data)
 
 // create product API
-export const createProductApi = (data) => Api.post('/api/product/create',data)
+export const createProductApi = (data) => Api.post('/api/product/create', data)
 
 // get all products API
-export const getAllProductsApi = () => Api.get('/api/product/get_all_products')
+export const getAllProductsApi = () => Api.get('/api/product/get_all_products', config)
 
 // get all single products API
-export const getSingleProductApi = (id) => Api.get(`/api/product/get_single_products/${id}`)
+export const getSingleProductApi = (id) => Api.get(`/api/product/get_single_products/${id}`, config)
 
 // delete product API
 export const deleteProduct = (id) => Api.delete(`/api/product/delete_product/${id}`)
 
 // update product API
-export const updateProduct = (id, data) => Api.put(`/api/product/update_product/${id}`,data)
+export const updateProduct = (id, data) => Api.put(`/api/product/update_product/${id}`, data)
 
 
 // http://localhost:5000/test
